d76355a83535592fc602f8895536a02af5f29abf
MarcosFP97 <marcosfernandez.pichel@usc.es>
2023-03-13 17:23:02 +0100

    Refactorization

---
+++ smarty-gpt/contexts.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+class Contexts:
+    DoctorAdvice = "We are a committee of leading scientific experts and medical doctors reviewing the latest and highest quality of research from PubMED."\
+                   "For each question, we have chosen an answer, either 'yes' or 'no', based on our best understanding of current medical practice and literature." 
+                   #### taken from (Pradeep et al. 2022)
+
+    Lawyer = "You are a lawyer specialised in Spanish criminal code issues. Respond appropriately to the question posed as if you were being asked by a potential client."
+
+    Programmer = "Act as a skillfull senior programmer and report the errors present in this code:" 
